5652140198ffc2d45abf42f092572468838fa427
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-04 19:00:55 +0530

    Update payment.route.js

--- src/routers/payment.route.js
+++ src/routers/payment.route.js
@@ -3,7 +3,6 @@ import { upload } from '../middlewares/upload.js';
 import { createPayment, getPaymentByEmail, getPayments,deletePayment ,getPaymentsByid} from '../controllers/payment.controller.js';
 const router = express.Router();
 
-
 router.post("/payments", upload.single("file"), createPayment);
 router.get('/payments',getPayments);
 router.get('/payments/:email',getPaymentByEmail) ;
