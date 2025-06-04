f076defbc018951347838e51fdb4fbc91c3f5438
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-04 19:00:30 +0530

    Update invoicesRouter.js

--- src/routers/invoicesRouter.js
+++ src/routers/invoicesRouter.js
@@ -6,13 +6,10 @@ import {
   getStudentFees,
   updateStudentFee
 } from '../controllers/invoiceCtrl.js';
-
 const router = express.Router();
-
 router.post('/payment-invoice', createStudentFee);
 router.get('/payment-invoice', getStudentFees);
 router.get('/payment-invoice/:id', getStudentFeeById);
 router.put('/payment-invoice/:id', updateStudentFee);
 router.delete('/payment-invoice/:id', deleteStudentFee);
-
 export default router;
