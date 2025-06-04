5f05273e20a354d7aa5256c15563e4ed76523ae3
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-04 18:59:07 +0530

    Update auth.routes.js

--- src/routers/auth.routes.js
+++ src/routers/auth.routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { upload } from '../middlewares/upload.js';
 import { register, login, createStudent, getAllStudents, getStudentById, deleteStudent, changeNewPassword, updateUser, getuserById, getAllByRoles } from '../controllers/auth.controller.js';
-import { authenticate } from '../middlewares/auth.middleware.js';
+// import { authenticate } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
@@ -9,12 +9,23 @@ router.post('/register', register);
 router.post('/login', login);
 router.get('/getUser/:id',getuserById)
 router.post('/changePassword/:id',changeNewPassword);
-router.post('/createStudent',authenticate, upload.fields([
+// router.post('/createStudent',authenticate, upload.fields([
+//     { name: 'photo', maxCount: 1 },
+//     { name: 'documents', maxCount: 1 }
+//   ]), createStudent);
+
+
+router.post('/createStudent', upload.fields([
     { name: 'photo', maxCount: 1 },
     { name: 'documents', maxCount: 1 }
   ]), createStudent);
+
+
+
+// router.get('/getStudentById/:id',authenticate, getStudentById);
+router.get('/getStudentById/:id', getStudentById);
 router.get('/getAllStudents',getAllStudents);
-router.get('/getStudentById/:id',authenticate, getStudentById);
+
 router.put('/updateUser/:id', upload.fields([
   { name: 'photo', maxCount: 1 },
   { name: 'documents', maxCount: 1 }
