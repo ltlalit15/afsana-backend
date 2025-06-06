7013faffafa49a47f39376c1b1ab633c1dd838bb
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-06 17:11:16 +0530

    Update remainderRouter.js

--- src/routers/remainderRouter.js
+++ src/routers/remainderRouter.js
@@ -8,20 +8,14 @@ import {
 } from '../controllers/remainderCtrl.js';
 
 const router = express.Router();
-
 // POST - Create new reminder
 router.post('/remainder', createReminder);
-
 // GET - Get all reminders
 router.get('/remainder', getReminders);
-
 // GET - Get a single reminder by ID
-router.get('/remainder/:id', getReminderById);
-
+router.get('/remainder/:id', getReminderById); 
 // PUT - Update a reminder
 router.put('/remainder/:id', updateReminder);
-
 // DELETE - Delete a reminder
 router.delete('/remainder/:id', deleteReminder);
-
 export default router;
