12c5d97e4c32ac2968fbd4707290797eadd67e20
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-06 17:10:41 +0530

    Update lead.router.js

--- src/routers/lead.router.js
+++ src/routers/lead.router.js
@@ -1,11 +1,18 @@
 import express from 'express';
-import { createLead, deleteLead, getLeadByCounselorId, getLeadById, getLeads, updateLead } from '../controllers/lead.controller.js';
+import { createLead, deleteLead, getLeadByCounselorId, getLeadById, getLeads, updateLead, getLeadByCounselorIdnew , updatePaymentStatus ,
+updateLeadStatus, invoicestatus} from '../controllers/lead.controller.js';
 const router = express.Router();
-
 router.post('/lead',createLead);
 router.get('/lead', getLeads);
 router.get('/lead/:id', getLeadById);
 router.get('/lead/counselor/:id', getLeadByCounselorId);
+router.get('/lead/getLeadByCounselorIdnew/:id', getLeadByCounselorIdnew);
 router.put('/lead/:id', updateLead);
 router.delete('/lead/:id', deleteLead);
+router.patch('/fee/update-status', updatePaymentStatus);
+router.patch('/fee/update-lesd-status',updateLeadStatus);
+router.patch('/fee/update-invoicestatus',invoicestatus);
+
+
+
 export default router;
