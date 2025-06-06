3d16c133e2f0f002c98a5c53b6ef8725b1b51f69
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-06-06 17:10:21 +0530

    Update inquiry.route.js

--- src/routers/inquiry.route.js
+++ src/routers/inquiry.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { createInquiry, deleteInquiry, getAllInquiries, getInquiryById, updateInquiry } from '../controllers/inquiry.controller.js';
-import { authenticate } from '../middlewares/auth.middleware.js';
+import { createInquiry, deleteInquiry, getAllInquiries, getInquiryById, updateInquiry, assignInquiry } from '../controllers/inquiry.controller.js';
+// import { authenticate } from '../middlewares/auth.middleware.js';
 const router = express.Router();
 router.post('/inquiries', createInquiry);
 router.get('/inquiries/:id', getInquiryById);
 router.put('/inquiries/:id', updateInquiry);
 router.delete('/inquiries/:id', deleteInquiry); 
 router.get('/inquiries', getAllInquiries);
+
+router.post('/assign-inquiry', assignInquiry); // ✅ Add this line
+
+
+
 export default router;
