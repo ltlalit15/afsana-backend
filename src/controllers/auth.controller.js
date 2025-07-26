1272f45079d4d3265e2bbeb4959c72468de1ef14
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-26 14:48:31 +0530

    Update auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -61,7 +61,7 @@ export const login = async (req, res) => {
 
     const user = results[0];
     const match = await bcrypt.compare(password, user.password);
-    const token = jwt.sign({ id: user.id, role: user.role }, "dfsdfdsfdsg34345464543sdffdg#%$", { expiresIn: '12h' });
+    const token = jwt.sign({ id: user.id, role: user.role }, "dfsdfdsfdsg34345464543sdffdg#%$", { expiresIn: '7d' });
     // const token = jwt.sign({ id: user.id, role: user.role },process.env.JWT_SECRET, { expiresIn: '12h' }
     // );
 
